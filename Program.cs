f117dac26764700b9dd9a72e9cd5b31c8fbbf39c
tessema2021 <ninafuchere@gmail.com>
2021-11-15 13:06:26 -0600

    phase#7

--- Program.cs
+++ Program.cs
@@ -1,12 +1,121 @@
 ﻿using System;
+Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+Console.WriteLine("How you doin?");
+Console.WriteLine("working");
 
-namespace EnthusiasticMoose
+Main();
+void Main()
 {
-    class Program
+
+    void MooseSays(string message)
     {
-        static void Main(string[] args)
+        Console.WriteLine($@"
+                                      _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    {message}
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+    ");
+    }
+
+    bool MooseAsks(string question)
+    {
+        Console.Write($"{question} (Y/N): ");
+        string answer = Console.ReadLine().ToLower();
+
+        while (answer != "y" && answer != "n")
         {
-            Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+            Console.Write($"{question} (Y/N): ");
+            answer = Console.ReadLine().ToLower();
+        }
+
+        if (answer == "y")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
+    CanadaQuestion();
+
+
+    void CanadaQuestion()
+    {
+        bool isTrue = MooseAsks("Is Canada real?");
+        if (isTrue)
+        {
+            MooseSays("Really? It seems very unlikely.");
+        }
+        else
+        {
+            MooseSays("I  K N E W  I T !!!");
+        }
+    }
+    void EnthusiasticQuestion()
+    {
+        bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
+        if (isEnthusiastic)
+        {
+            MooseSays("Yay!");
+        }
+        else
+        {
+            MooseSays("You should try it!");
+        }
+    }
+
+    void LoveCSharpQuestion()
+    {
+        bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
+        if (doesLoveCSharp)
+        {
+            MooseSays("Good job sucking up to your instructor!");
+        }
+        else
+        {
+            MooseSays("You will...oh, yes, you will...");
+        }
+    }
+
+    void SecretQuestion()
+    {
+        bool wantsSecret = MooseAsks("Do you want to know a secret?");
+        if (wantsSecret)
+        {
+            MooseSays("ME TOO!!!! I love secrets...tell me one!");
+        }
+        else
+        {
+            MooseSays("Oh, no...secrets are the best, I love to share them!");
+        }
+    }
+
+    CanadaQuestion();
+    EnthusiasticQuestion();
+    LoveCSharpQuestion();
+    SecretQuestion();
 }
+Main();
