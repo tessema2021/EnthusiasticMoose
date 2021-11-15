eb3e14ab3d487b35bd8941616470f384804c1a6b
tessema2021 <ninafuchere@gmail.com>
2021-11-14 19:43:38 -0600

    phase#4 done

--- Program.cs
+++ Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 
-namespace EnthusiasticMoose
+
+void MooseSays(string message)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
-        }
-    }
+    Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+    Console.WriteLine("H I, I'M  E N T H U S I A S T I C !");
+    Console.WriteLine("I really am enthusiastic");
+
+    // Let the moose speak!
+
+
+
+    Console.WriteLine($@"
+    
+                                      _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    {message}
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+    ");
+
 }
+MooseSays("the messeage");
+
+
